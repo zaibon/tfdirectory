5435446a3135bf7d6cfb65daccab0b03ff8814fd
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-04-17 20:58:14 +0200

    Gracefull shutdown for server + add some middlware
    middleware for
      - panic recovery
      - logging
      - gzip

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/zaibon/tfdirectory/mongo"
 	"github.com/zaibon/tfdirectory/server"
@@ -22,5 +25,28 @@ func main() {
 	fs := mongo.NewFarmerService(ms, "tfdirectory", "farmer")
 	s := server.NewServer(us, fs)
 
+	// start the http server
 	s.Start(":8081")
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for the http server to stop.
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	go func() {
+		log.Println("shutting down")
+		s.Shutdown(ctx)
+	}()
+
+	<-ctx.Done()
+	log.Println("server is shutdown")
+
+	os.Exit(0)
 }
--- server/server.go
+++ server/server.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -11,21 +13,47 @@ import (
 )
 
 type Server struct {
+	srv    *http.Server
 	router *mux.Router
 }
 
 func NewServer(n tfdirectory.NodeService, f tfdirectory.FarmerService) *Server {
-	s := Server{router: mux.NewRouter()}
+	r := mux.NewRouter()
+	srv := &http.Server{
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
+	}
+	s := Server{
+		router: r,
+		srv:    srv,
+	}
 	NewUserRouter(n, s.newSubrouter("/nodes"))
 	NewFarmerRouter(f, s.newSubrouter("/farmers"))
 	return &s
 }
 
+// Start starts the server and make it listen to listen address
 func (s *Server) Start(listen string) {
 	log.Printf("Listening on %s\n", listen)
-	if err := http.ListenAndServe(listen, handlers.LoggingHandler(os.Stdout, s.router)); err != nil {
-		log.Fatal("http.ListenAndServe: ", err)
-	}
+
+	s.router.Use(handlers.CompressHandler)
+	s.router.Use(func(h http.Handler) http.Handler {
+		return handlers.LoggingHandler(os.Stdout, h)
+	})
+	s.router.Use(handlers.RecoveryHandler())
+
+	s.srv.Addr = listen
+	go func() {
+		if err := s.srv.ListenAndServe(); err != nil {
+			log.Println("http.ListenAndServe: ", err)
+		}
+	}()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.srv.Shutdown(ctx)
 }
 
 func (s *Server) newSubrouter(path string) *mux.Router {
